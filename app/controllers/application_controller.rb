2fe6c48ce139d0b708541a58d4d3936ca65cf3ee
ysingh <yudi.singh@careerbuilder.com>
2016-09-16 15:33:40 -0400

    Cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,4 @@ require 'httparty'
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def index
-    render json: autocomplete
-  end
-
-  private
-
-  def autocomplete
-    base_url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json'
-    input = 'Vict'
-    types = 'cities'
-
-    HTTParty.get("#{ base_url }?input=#{ input }&types=(#{ types })&language=en_US&key=#{ ENV['PLACES_API_KEY'] }")
-  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,14 @@ class HomeController < ApplicationController
 
   def form
   end
+
+  private
+
+  def autocomplete
+    base_url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json'
+    input = 'Vict'
+    types = 'cities'
+
+    HTTParty.get("#{ base_url }?input=#{ input }&types=(#{ types })&language=en_US&key=#{ ENV['PLACES_API_KEY'] }")
+  end
 end
