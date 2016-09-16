1be2626b1c28b157641091b0ca8d8893bf83bee6
ysingh <yudi.singh@careerbuilder.com>
2016-09-16 15:22:01 -0400

    Working api call, setup .env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
+require 'httparty'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    render html: "hello, yudi"
+    render json: autocomplete
+  end
+
+  private
+
+  def autocomplete
+    base_url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json'
+    input = 'Vict'
+    types = 'cities'
+
+    HTTParty.get("#{ base_url }?input=#{ input }&types=(#{ types })&language=en_US&key=#{ ENV['PLACES_API_KEY'] }")
   end
 end
